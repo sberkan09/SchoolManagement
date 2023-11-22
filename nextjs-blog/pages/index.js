409d9a6308263c4ef884cc9f350b4e62c45fe10f
sberkan09 <samiakkaya396@gmail.com>
2023-11-23 00:19:49 +0300

    Template created with framework Next.js

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+
+function Exciting() {
+  return <h1>Exciting</h1>
+}
+
+export default function Home() {
+  return (
+    Exciting()
+  );
+}
