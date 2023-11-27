f8e63024ba9c1d1bad08f783130e4285dd760240
sberkan09 <samiakkaya396@gmail.com>
2023-11-27 15:54:12 +0300

    api connection

--- client/src/pages/Ogrenci.js
+++ client/src/pages/Ogrenci.js
@@ -2,49 +2,49 @@ import React from 'react';
 import axios from 'axios';
 
 function OgrenciList() {
-  const [ogrenci, setOgrenci] = React.useState(null);
+  const [rows, setRows] = React.useState([]);
   React.useEffect(() => {
     (async function getData() {
       await axios.get('http://localhost:3006/api/ogrenci/ogrencileriGetir').then((response) => {
-        setOgrenci(response.data);
+        setRows(response.data);
       });
     }());
   }, []);
 
-  if (!ogrenci) return null;
-
-  console.log(ogrenci);
-
   return (
-    <div>
-      <div>
-        <p>
-          TC NO:
-          {ogrenci.TC_NO}
-        </p>
-        <p>
-          İsim:
-          {ogrenci.ISIM}
-        </p>
-        <p>
-          Soyisim:
-          {ogrenci.SOYISIM}
-        </p>
-        <p>
-          Adres:
-          {ogrenci.ADRES}
-        </p>
-        <p>
-          Tel No:
-          {ogrenci.TEL_NO}
-        </p>
-        <p>
-          E-Posta:
-          {ogrenci.E_POSTA}
-        </p>
-        <br />
-      </div>
-    </div>
+    <>
+      {rows.map((row) => (
+        <div>
+          <div>
+            <p>
+              TC NO:
+              {row.TC_NO}
+            </p>
+            <p>
+              İsim:
+              {row.ISIM}
+            </p>
+            <p>
+              Soyisim:
+              {row.SOYISIM}
+            </p>
+            <p>
+              Adres:
+              {row.ADRES}
+            </p>
+            <p>
+              Tel No:
+              {row.TEL_NO}
+            </p>
+            <p>
+              E-Posta:
+              {row.E_POSTA}
+            </p>
+            <br />
+          </div>
+        </div>
+      ))}
+    </>
   );
 }
 
