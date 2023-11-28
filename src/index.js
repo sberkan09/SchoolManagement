1df5d52c43d56fe1be2895690a1cdf8ad2d2f69d
ilkeryasincakir <ilkeryasincakir8@gmail.com>
2023-11-28 19:28:44 +0300

    3 endpoints added.

--- src/index.js
+++ src/index.js
@@ -338,6 +338,39 @@ app.get("/api/calisan/idareciGetir", (req, res) => {
 	}
 });
 
+app.post("/api/calisan/idareciEkle", (req, res) => {
+	const { TC_NO, ISIM, SOYISIM, ADRES, TEL_NO, E_POSTA } = req.query;
+
+	// Girdilerin doğrulanması ve temizlenmesi
+	// NOT: Bu örnek, girdilerin doğrulanmasını ve temizlenmesini göstermek için basitleştirilmiştir.
+	// Gerçek bir uygulamada daha güçlü doğrulama ve temizleme işlemleri yapılmalıdır.
+	if (TC_NO && ISIM && SOYISIM && ADRES && TEL_NO && E_POSTA) {
+		const query1 = `INSERT INTO calisan (TC_NO, ISIM, SOYISIM, ADRES, TEL_NO, E_POSTA) VALUES ('${TC_NO}', '${ISIM}', '${SOYISIM}', '${ADRES}','${TEL_NO}','${E_POSTA}');`;
+
+		db.query(query1, (error, results) => {
+			if (error) {
+				console.error(error);
+				res.status(500).send("Error while inserting into calisan");
+				return;
+			}
+
+			const query2 = `INSERT INTO idari (TC_NO) VALUES ('${TC_NO}');`;
+
+			db.query(query2, (error, results) => {
+				if (error) {
+					console.error(error);
+					res.status(500).send("Error while inserting into idari");
+					return;
+				}
+
+				res.status(200).send("idareci added successfully");
+			});
+		});
+	} else {
+		res.status(400).send("Missing or invalid parameters");
+	}
+});
+
 //ogretmenleri getir
 app.get("/api/calisan/ogretmenGetir", (req, res) => {
 	const { TC_NO, PART_MI } = req.query;
@@ -378,6 +411,70 @@ app.get("/api/calisan/ogretmenGetir", (req, res) => {
 	}
 });
 
+///api/calisan/fullTime/ogretmenEkle
+app.post("/api/calisan/fullTime/OgretmenEkle", (req, res) => {
+	const { TC_NO, ISIM, SOYISIM, ADRES, TEL_NO, E_POSTA, MAAS } = req.body;
+	const PART_MI = false; // Tam zamanlı öğretmenler için false olarak ayarlanır
+
+	// calisan tablosuna ekleme
+	db.query(
+		`INSERT INTO calisan (TC_NO, ISIM, SOYISIM, ADRES, TEL_NO, E_POSTA) VALUES (${TC_NO}, ${ISIM}, ${SOYISIM}, ${ADRES}, ${TEL_NO}, ${E_POSTA});`
+	)
+		.then(() => {
+			// ogretmen tablosuna ekleme
+			return db.query(
+				`INSERT INTO ogretmen (TC_NO, PART_MI) VALUES (${TC_NO}, ${PART_MI});`,
+				[TC_NO, PART_MI]
+			);
+		})
+		.then(() => {
+			// full_timer tablosuna ekleme
+			return db.query(
+				`INSERT INTO full_timer (TC_NO, MAAS) VALUES (${TC_NO}, ${MAAS});`,
+				[TC_NO, MAAS]
+			);
+		})
+		.then(() => {
+			res.status(200).send("Tam zamanlı öğretmen başarıyla eklendi");
+		})
+		.catch((error) => {
+			console.error("/api/calisan/fullTime/OgretmenEkle", error);
+			res.status(500).send("Öğretmen eklenirken bir hata oluştu");
+		});
+});
+
+//api/calisan/partTime/ogretmenEkle
+app.post("/api/calisan/partTime/OgretmenEkle", (req, res) => {
+	const { TC_NO, ISIM, SOYISIM, ADRES, TEL_NO, E_POSTA, SAAT } = req.body;
+	const PART_MI = true; // Yarı zamanlı öğretmenler için true olarak ayarlanır
+
+	// calisan tablosuna ekleme
+	db.query(
+		`INSERT INTO calisan (TC_NO, ISIM, SOYISIM, ADRES, TEL_NO, E_POSTA) VALUES (${TC_NO}, ${ISIM}, ${SOYISIM},${ADRES}, ${TEL_NO}, ${E_POSTA});`
+	)
+		.then(() => {
+			// ogretmen tablosuna ekleme
+			return db.query(
+				`INSERT INTO ogretmen (TC_NO, PART_MI) VALUES (${TC_NO}, ${PART_MI});`,
+				[TC_NO, PART_MI]
+			);
+		})
+		.then(() => {
+			// part_timer tablosuna ekleme
+			return db.query(
+				`INSERT INTO part_timer (TC_NO, SAAT) VALUES (${TC_NO}, ${SAAT});`,
+				[TC_NO, SAAT]
+			);
+		})
+		.then(() => {
+			res.status(200).send("Yarı zamanlı öğretmen başarıyla eklendi");
+		})
+		.catch((error) => {
+			console.error("/api/calisan/partTime/OgretmenEkle", error);
+			res.status(500).send("Öğretmen eklenirken bir hata oluştu");
+		});
+});
+
 //temizlik personellerini getir
 app.get("/api/calisan/temizlikGetir", (req, res) => {
 	const { TC_NO } = req.query;
@@ -400,6 +497,30 @@ app.get("/api/calisan/temizlikGetir", (req, res) => {
 				console.error(`/api/calisan/temizlikGetir = '${TC_NO}'`, error)
 			);
 	}
+});
+
+// api/calisan/temizlikciEkle
+app.post("/api/calisan/temizlikciEkle", (req, res) => {
+	const { TC_NO, ISIM, SOYISIM, ADRES, TEL_NO, E_POSTA } = req.body;
+
+	// calisan tablosuna ekleme
+	db.query(
+		`INSERT INTO calisan (TC_NO, ISIM, SOYISIM, ADRES, TEL_NO, E_POSTA) VALUES (${TC_NO}, ${ISIM}, ${SOYISIM}, ${ADRES}, ${TEL_NO}, ${E_POSTA});`,
+		[TC_NO, ISIM, SOYISIM, ADRES, TEL_NO, E_POSTA]
+	)
+		.then(() => {
+			// temizlikci tablosuna ekleme
+			return db.query(`INSERT INTO temizlikci (TC_NO) VALUES (${TC_NO});`, [
+				TC_NO,
+			]);
+		})
+		.then(() => {
+			res.status(200).send("Temizlikçi başarıyla eklendi");
+		})
+		.catch((error) => {
+			console.error("/api/calisan/temizlikciEkle", error);
+			res.status(500).send("Temizlikçi eklenirken bir hata oluştu");
+		});
 });
 
 //Ogrenci TC_NOya gore velilerini getir
@@ -643,6 +764,15 @@ app.get("/api/ders/dersSaatiUygunGetir", (req, res) => {
 			})
 			.catch((error) => console.error("/api/ders/dersSaatiUygunGetir", error));
 	}
+});
+
+// /api/gider/tumGiderleriGetir
+app.get("/api/gider/tumGiderleriGetir", (req, res) => {
+	db.query(`SELECT * FROM gider;`)
+		.then((data) => {
+			res.json(data[0]);
+		})
+		.catch((error) => console.error("/api/gider/tumGiderleriGetir", error));
 });
 
 //sabit giderleri getir
